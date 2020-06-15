1cb3576f0cb9ddfa10a475e458de56281f502a2c
Chip Anderson <chip.anderson@lwtech.edu>
2020-06-15 13:08:34 -0700

    Removed startDate from this version

--- src/main/java/edu/lwtech/csd299/topten/Member.java
+++ src/main/java/edu/lwtech/csd299/topten/Member.java
@@ -1,13 +1,10 @@
 package edu.lwtech.csd299.topten;
 
-import java.util.Date;
-
 public class Member {
     
     private int id;             // Database ID (or -1 if it isn't in the database yet)
     private String email;
     private String password;
-    private Date startDate;
 
     public Member(String email, String password) {
         this(-1, email, password);
@@ -25,7 +22,6 @@ public class Member {
         this.id = id;
         this.email = email;
         this.password = password;
-        this.startDate = new Date();
     }
 
     public int getID() {
@@ -44,13 +40,9 @@ public class Member {
         return password;
     }
 
-    public Date getStartDate() {
-        return startDate;
-    }
-
     @Override
     public String toString() {
-        return "[" + email + ", ******** (" + startDate + ")]";
+        return "[" + email + ", ******** ]";
     }
 
 }
